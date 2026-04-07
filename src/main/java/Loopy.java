4fca5dde30823ed5368c3faeef066b3ee972bbf0
Danielle Hoffman <daniellehoffman537@gmail.com>
2026-04-07 13:24:18 +0200

    Loops

--- src/main/java/Loopy.java
+++ src/main/java/Loopy.java
@@ -1,7 +1,12 @@
 public class Loopy {
     static void main(String[] args) {
-        for (int counter = 1; counter <= 5; counter++){
-            System.out.println(counter);
+        int interest = calculateInterest(10000.0,2.00);
+        System.out.println("R10 000 at 2% interest is " + interest);
+
         }
-    }
+
+        }
+        public static double calculateInterest(double amount, double interestrate){
+            return (amount * (interestrate / 100));
+        }
 }
